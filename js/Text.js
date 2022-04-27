c50f6fed05fdabe894096669efa472184291d0a1
Bread traversy <mar@breadtraversy.com>
2022-04-27 23:43:40 +0200

    first commit

---
+++ js/Text.js
@@ -0,0 +1,23 @@
+// $d(document).ready(function(){
+//     console.log("is j")
+// }
+// )
+// $d(document).ready(function(){
+//     $('div').show()
+// })
+// $d(document).ready(function(){
+//     $('div').fadeIn()
+// })
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:2,
+        loop:true,
+        nav:true,
+        margin:40,
+       
+        dots:false,
+       
+      
+
+    });
+  });
